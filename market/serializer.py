29000d8adde2ecb30399cd41ff0894f22fd2ba68
Кирилл <kkulikov14@list.ru>
2022-05-29 16:10:54 +0300

    for docker

--- market/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Lot
-
-
-class LotSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lot
-        fields = ['__all__']
